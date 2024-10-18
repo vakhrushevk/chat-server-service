f4e0889f97592830bb07f478813d429667a6fb03
Vakhrushev <konstantinvakhrushev@Noutbuk-Konstantin.local>
2024-10-18 16:38:59 +0500

    add grpc config pg config

---
+++ internal/config/env/pg.go
@@ -0,0 +1,33 @@
+package env
+
+import (
+	"errors"
+	"github.com/vakhrushevk/chat-server-service/internal/config"
+	"os"
+)
+
+var _ config.PgConfig = (*pgConfig)(nil)
+
+const (
+	dnsEnvName = "PG_DSN"
+)
+
+type pgConfig struct {
+	dsn string
+}
+
+// NewPGConfig создает и инициализирует конфигурацию для подключения к бд
+func NewPGConfig() (config.PgConfig, error) {
+	dsn := os.Getenv(dnsEnvName)
+	if len(dsn) == 0 {
+		return nil, errors.New("pg dsn not found")
+	}
+	return &pgConfig{
+		dsn: dsn,
+	}, nil
+}
+
+// DSN - Возвращает строку подключения к бд
+func (p *pgConfig) DSN() string {
+	return p.dsn
+}
